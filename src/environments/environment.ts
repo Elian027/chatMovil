775592ba2b4e5f89883f31e753a29cf2a185099c
Elian027 <elian.moreira@epn.edu.ec>
2023-12-04 21:02:49 -0500

    splash screen

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBHm6bZfsTW5Z7TJh0Out-2rKj2_is21oU",
+    authDomain: "myapp-a675b.firebaseapp.com",
+    databaseURL: "https://myapp-a675b-default-rtdb.firebaseio.com",
+    projectId: "myapp-a675b",
+    storageBucket: "myapp-a675b.appspot.com",
+    messagingSenderId: "562726371770",
+    appId: "1:562726371770:web:1d38834f50641cc574dd83",
+    measurementId: "G-WG5FZF6Y9V"
+  }
+};
